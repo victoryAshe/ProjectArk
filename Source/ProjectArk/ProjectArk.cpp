6401ee6852f0622ad12bd14ef0fa9a52252149fa
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2024-03-22 15:58:59 +0900

    Initial commit

---
+++ Source/ProjectArk/ProjectArk.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectArk.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectArk, "ProjectArk" );
+
+DEFINE_LOG_CATEGORY(LogProjectArk)
+ 
